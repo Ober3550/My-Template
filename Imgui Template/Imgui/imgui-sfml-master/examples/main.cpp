5a9e7a5b17baba10047f830458a6a9e4b81d5978
Ober3550 <41331378+Ober3550@users.noreply.github.com>
2020-07-19 23:46:42 +1000

    First Commit

---
+++ Imgui Template/Imgui/imgui-sfml-master/examples/main.cpp
@@ -0,0 +1,55 @@
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include "zlib.h"
+
+#include "absl/container/flat_hash_map.h"
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    absl::flat_hash_map<std::string, std::string> ducks = {{"a", "huey"}, {"b", "dewey"}, {"c", "louie"}};
+    std::string search_key = "b";
+    auto result = ducks.find(search_key);
+
+    if (result != ducks.end()) {
+        std::cout << "Result: " << result->second << std::endl;
+    }
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        //ImGui::ShowTestWindow();
+        
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+    
+    return 0;
+}
---
+++ Imgui Template/abseil/abseil-cpp-master/absl/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ober3/source/repos/Imgui Template/Imgui Template/abseil/abseil-cpp-master
+# Build directory: C:/Users/ober3/source/repos/Imgui Template/Imgui Template/abseil/abseil-cpp-master/absl
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest-build")
+subdirs("absl")
