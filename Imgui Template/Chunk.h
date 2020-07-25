697f3b952e71f2bcfced4cbd8d96adf4368b960a
Ober3550 <41331378+Ober3550@users.noreply.github.com>
2020-07-25 19:07:28 +1000

    Rearranged the project

--- Imgui Template/Chunk.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Constants.h"
-struct Chunk
-{
-	uint8_t tiles[1024] = {};
-};
--- Imgui Template/Facing.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <cstdint>
-enum Facing : uint8_t {
-	north = 0, east = 1, south = 2, west = 3
-};
