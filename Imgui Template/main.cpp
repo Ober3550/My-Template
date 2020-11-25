8ecb5f4996f42cd2dee03909139b169a8a7002b5
Ober3550 <Ober3550@gmail.com>
2020-11-25 22:42:47 +1000

    Added floor back

--- Imgui Template/main.cpp
+++ Imgui Template/main.cpp
@@ -194,7 +194,7 @@ int main()
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glLoadMatrixf(glm::value_ptr(glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp)));
 
-            if (false)
+            //if (false)
             {
                 // Draw a white grid "floor" for the tetrahedron to sit on.
                 glColor3f(1.0, 1.0, 1.0);
